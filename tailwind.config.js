19b331ea9e1ebb29841c9d6c5c60949dfa4c987f
mitch44hart <mitch44hart@gmail.com>
2025-04-23 02:17:00 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./index.html', './*.js'],
-    theme: {
-      extend: {
-        colors: {
-          primary: {
-            DEFAULT: '#22c55e',
-            dark: '#16a34a',
-            light: '#4ade80'
-          }
+  content: ['./index.html', './*.js'],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#22c55e',
+          dark: '#16a34a',
+          light: '#4ade80'
         }
       }
-    },
-    plugins: []
-  };
+    }
+  },
+  plugins: []
+};
